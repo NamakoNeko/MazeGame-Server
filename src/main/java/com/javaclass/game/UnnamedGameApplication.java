4797943c534b0b0abea898b809955e5862171fb0
NamakoNeko <brian.hu0516@gmail.com>
2026-04-21 21:18:59 +0800

    新增專案資料夾

---
+++ src/main/java/com/javaclass/game/UnnamedGameApplication.java
@@ -0,0 +1,13 @@
+package com.javaclass.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnnamedGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnnamedGameApplication.class, args);
+	}
+
+}
